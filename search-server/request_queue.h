8aced3f82122fb28290f77f5f000cbc53e6622a3
aleks070582 <aleks.ryzhov82@gmail.com>
2024-11-27 06:12:34 +0400

    поправил

--- search-server/request_queue.h
+++ search-server/request_queue.h
@@ -1,46 +1,40 @@
-#pragma once
+#include "request_queue.h"
 
-#include<deque>
-#include"search_server.h"
-#include"document.h"
+using namespace std;
 
+vector<Document> RequestQueue::AddFindRequest(const string & raw_query, DocumentStatus status) {
+    vector<Document> document = request_server_.FindTopDocuments(raw_query, status);
 
-class RequestQueue {
-public:
-    explicit RequestQueue(const SearchServer& search_server) :request_server_(search_server)
-    {
-
-    }
-    
-    template <typename DocumentPredicate>
-    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate);
-    
-   std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status);
+    requests_.push_back({ !document.empty() });
   
-    std::vector<Document> AddFindRequest(const std::string& raw_query);
-    int GetNoResultRequests() const;
    
-private:
-    struct QueryResult
-    {
-        std::string raw;
-        std::vector<Document> doc;
-        
-    };
-    std::deque<QueryResult> requests_;
-    const static int min_in_day_ = 1440;
-    const SearchServer& request_server_;
-    
-};
-
-template <typename DocumentPredicate>
-std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
-   std::vector<Document> document = request_server_.FindTopDocuments(raw_query, document_predicate);
-
-    requests_.push_back({ raw_query,document });
     if (requests_.size() > min_in_day_)
     {
         requests_.pop_front();
     }
     return document;
+}
+
+vector<Document> RequestQueue::AddFindRequest(const string & raw_query) {
+    vector<Document> document = request_server_.FindTopDocuments(raw_query);
+    requests_.push_back({! document.empty() });
+
+    if (requests_.size() > min_in_day_)
+    {
+        requests_.pop_front();
+    }
+    return document;
+}
+
+int RequestQueue::GetNoResultRequests() const
+{
+    int result = 0;
+    for (const QueryResult& ch : requests_)
+    {
+        if (ch.result==false)
+        {
+            ++result;
+        }
+    }
+    return result;
 }
