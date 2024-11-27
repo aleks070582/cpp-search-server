95b65c7c681912d0ea63dec072d4456d26cbad46
aleks070582 <aleks.ryzhov82@gmail.com>
2024-11-27 06:15:15 +0400

    поправил

--- search-server/request_queue.cpp
+++ search-server/request_queue.cpp
@@ -1,9 +1,13 @@
 #include "request_queue.h"
+
 using namespace std;
 
 vector<Document> RequestQueue::AddFindRequest(const string & raw_query, DocumentStatus status) {
     vector<Document> document = request_server_.FindTopDocuments(raw_query, status);
-    requests_.push_back({ raw_query,document });
+
+    requests_.push_back({ !document.empty() });
+  
+   
     if (requests_.size() > min_in_day_)
     {
         requests_.pop_front();
@@ -13,7 +17,8 @@ vector<Document> RequestQueue::AddFindRequest(const string & raw_query, Document
 
 vector<Document> RequestQueue::AddFindRequest(const string & raw_query) {
     vector<Document> document = request_server_.FindTopDocuments(raw_query);
-    requests_.push_back({ raw_query,document });
+    requests_.push_back({! document.empty() });
+
     if (requests_.size() > min_in_day_)
     {
         requests_.pop_front();
@@ -26,7 +31,26 @@ int RequestQueue::GetNoResultRequests() const
     int result = 0;
     for (const QueryResult& ch : requests_)
     {
-        if (ch.doc.empty())
+        if (ch.result==false)
+        {
+            ++result;
+        }
+    }
+    return result;
+}
+    if (requests_.size() > min_in_day_)
+    {
+        requests_.pop_front();
+    }
+    return document;
+}
+
+int RequestQueue::GetNoResultRequests() const
+{
+    int result = 0;
+    for (const QueryResult& ch : requests_)
+    {
+        if (ch.result==false)
         {
             ++result;
         }
