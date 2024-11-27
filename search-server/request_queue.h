160d63e81044e49f89b23a585b7537f98d49a5c2
aleks070582 <aleks.ryzhov82@gmail.com>
2024-11-27 06:21:45 +0400

    поправил но что то не то с проверкой на сайте проходит и при использовании  requests_.push_back({ document.empty() } и  requests_.push_back({! document.empty() } хотя в cpp везде используется  requests_.push_back({! document.empty() }

--- search-server/request_queue.h
+++ search-server/request_queue.h
@@ -1,40 +1,46 @@
-#include "request_queue.h"
+#pragma once
 
-using namespace std;
+#include<deque>
+#include"search_server.h"
+#include"document.h"
 
-vector<Document> RequestQueue::AddFindRequest(const string & raw_query, DocumentStatus status) {
-    vector<Document> document = request_server_.FindTopDocuments(raw_query, status);
 
-    requests_.push_back({ !document.empty() });
-  
-   
-    if (requests_.size() > min_in_day_)
+class RequestQueue {
+public:
+    explicit RequestQueue(const SearchServer& search_server) :request_server_(search_server)
     {
-        requests_.pop_front();
-    }
-    return document;
-}
 
-vector<Document> RequestQueue::AddFindRequest(const string & raw_query) {
-    vector<Document> document = request_server_.FindTopDocuments(raw_query);
+    }
+
+    template <typename DocumentPredicate>
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate);
+
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status);
+
+    std::vector<Document> AddFindRequest(const std::string& raw_query);
+    int GetNoResultRequests() const;
+
+private:
+    struct QueryResult
+    {
+        bool result;
+
+    };
+    std::deque<QueryResult> requests_;
+    const static int min_in_day_ = 1440;
+    const SearchServer& request_server_;
+
+};
+
+template <typename DocumentPredicate>
+std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
+    std::vector<Document> document = request_server_.FindTopDocuments(raw_query, document_predicate);
+    
+    
     requests_.push_back({! document.empty() });
-
     if (requests_.size() > min_in_day_)
     {
         requests_.pop_front();
     }
     return document;
-}
-
-int RequestQueue::GetNoResultRequests() const
-{
-    int result = 0;
-    for (const QueryResult& ch : requests_)
-    {
-        if (ch.result==false)
-        {
-            ++result;
-        }
-    }
-    return result;
 }
